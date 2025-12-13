2cf2148c471826c6d9620e2e313db570abc61d4d
eriinys <eriinys@duck.com>
2025-12-12 22:18:37 -0600

    Added new unit test for MySqlProductDao search method bug fix

--- backend-api/src/test/java/org/yearup/data/mysql/MySqlProductDaoTest.java
+++ backend-api/src/test/java/org/yearup/data/mysql/MySqlProductDaoTest.java
@@ -1,14 +1,25 @@
 package org.yearup.data.mysql;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.yearup.controllers.ProductsController;
+import org.yearup.data.ProductDao;
 import org.yearup.models.Product;
-
+import java.util.*;
 import java.math.BigDecimal;
-import java.sql.SQLException;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.mockito.Mockito.*;
+import static org.mockito.ArgumentMatchers.any;
 
 class MySqlProductDaoTest extends BaseDaoTestClass
 {
@@ -45,4 +56,25 @@ class MySqlProductDaoTest extends BaseDaoTestClass
         assertEquals(expected.getPrice(), actual.getPrice(), "Because I tried to get product 1 from the database.");
     }
 
+    //tests for bug fix #1
+    @Test
+    public void searchShouldReturnCorrectProducts(){
+        //arange
+        Integer categoryId = 3;
+        BigDecimal minPrice = BigDecimal.valueOf(80);
+        BigDecimal maxPrice = BigDecimal.valueOf(90);
+        String subCategory = "";
+
+        //act
+        List<Product> result = dao.search(categoryId, minPrice, maxPrice, subCategory);
+
+        //assert
+        assertNotNull(result);
+        for(Product p : result){
+            assertEquals(3, p.getCategoryId());
+            assertTrue(p.getPrice().compareTo(minPrice) >= 0); //product price is greater or equal to minPrice
+            assertTrue(p.getPrice().compareTo(maxPrice) <= 0); //product price is less than or equal to maxPrice
+        }
+    }
 }
+
