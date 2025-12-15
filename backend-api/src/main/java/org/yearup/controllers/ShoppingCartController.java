e4fc53f3a1c6fc50465352ea300e1ccff7ff2098
eriinys <eriinys@duck.com>
2025-12-14 18:03:52 -0600

    Added controller mapping and updated getCart to return ShoppingCart by userId. Added @PreAuthorize to block unauthenticated request

--- backend-api/src/main/java/org/yearup/controllers/ShoppingCartController.java
+++ backend-api/src/main/java/org/yearup/controllers/ShoppingCartController.java
@@ -1,6 +1,12 @@
 package org.yearup.controllers;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 import org.yearup.data.ProductDao;
 import org.yearup.data.ShoppingCartDao;
@@ -12,6 +18,9 @@ import java.security.Principal;
 
 // convert this class to a REST controller
 // only logged in users should have access to these actions
+@RestController
+@RequestMapping("cart") //when HTTP request is received in this path /cart, Spring calls the methods in this controller
+@CrossOrigin
 public class ShoppingCartController
 {
     // a shopping cart requires
@@ -19,10 +28,11 @@ public class ShoppingCartController
     private UserDao userDao;
     private ProductDao productDao;
 
-
-
     // each method in this controller requires a Principal object as a parameter
-    public ShoppingCart getCart(Principal principal)
+    //Principal object represent current user
+    @GetMapping
+    @PreAuthorize("hasRole('USER')")
+    public ResponseEntity<ShoppingCart> getCart(Principal principal)
     {
         try
         {
@@ -33,7 +43,9 @@ public class ShoppingCartController
             int userId = user.getId();
 
             // use the shoppingcartDao to get all items in the cart and return the cart
-            return null;
+            ShoppingCart cart = shoppingCartDao.getByUserId(userId);
+
+            return ResponseEntity.ok(cart); //throws status code 200 with body (cart)
         }
         catch(Exception e)
         {
