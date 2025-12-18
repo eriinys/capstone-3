654dc55ade70a196ba21676d2cb5cb200771da94
eriinys <eriinys@duck.com>
2025-12-17 21:15:38 -0600

    made change to method name

--- backend-api/src/main/java/org/yearup/data/ShoppingCartDao.java
+++ backend-api/src/main/java/org/yearup/data/ShoppingCartDao.java
@@ -1,12 +1,11 @@
 package org.yearup.data;
 
-import org.yearup.controllers.ShoppingCartController;
 import org.yearup.models.ShoppingCart;
 import org.yearup.models.ShoppingCartItem;
 
 public interface ShoppingCartDao
 {
     ShoppingCart getByUserId(int userId);
-    ShoppingCartItem addOrUpdateItem(int productId, int quantity, int userId);
+    ShoppingCartItem insertOrUpdate(int productId, int quantity, int userId);
     int deleteAllItem(int userId);
 }
