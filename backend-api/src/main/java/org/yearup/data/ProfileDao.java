09d91d757ea6a34f00647d752529b94a47783c8c
eriinys <eriinys@duck.com>
2025-12-18 14:19:30 -0600

    added getProfileById and updateProfile methods

--- backend-api/src/main/java/org/yearup/data/ProfileDao.java
+++ backend-api/src/main/java/org/yearup/data/ProfileDao.java
@@ -5,5 +5,7 @@ import org.yearup.models.Profile;
 
 public interface ProfileDao
 {
+    Profile getProfileById(int userId);
     Profile create(Profile profile);
+    Profile updateProfile(int userId, Profile profile);
 }
